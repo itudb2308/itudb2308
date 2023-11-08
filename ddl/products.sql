83ca0006498a07bc3532da8cba84cba49ff713a9
omer-faruk-erdem <mlomer931@gmail.com>
2023-11-08 21:52:14 +0300

    Fixed error in products.sql

--- ddl/products.sql
+++ ddl/products.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTs products
 (
-    id              INTEGER PRIMARY KEY,
-    cost            FLOAT,
-    category        VARCHAR(50),    -- Max length 29
-    name            VARCHAR(250),   -- Max length 224
-    brand           VARCHAR(50),    -- Max length 41
-    retail_price    FLOAT,
-    department      VARCHAR(5),     -- Max length 5
-    sku             VARCHAR(50),    -- Max length 32
-    category        INTEGER REFERENCES distribution_centers,
+    id                            INTEGER PRIMARY KEY,
+    cost                          FLOAT,
+    category                      VARCHAR(50),    -- Max length 29
+    name                          VARCHAR(250),   -- Max length 224
+    brand                         VARCHAR(50),    -- Max length 41
+    retail_price                  FLOAT,
+    department                    VARCHAR(5),     -- Max length 5
+    sku                           VARCHAR(50),    -- Max length 32
+    distribution_center_id        INTEGER REFERENCES distribution_centers,
 );
