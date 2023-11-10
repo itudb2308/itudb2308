34f18ee966eb072144fe97420a328b68abc83070
muratbiberoglu <muratbiberoglu61@hotmail.com>
2023-11-10 17:16:03 +0300

    fix bugs

--- src/repository/BaseRepository.py
+++ src/repository/BaseRepository.py
@@ -7,7 +7,7 @@ class BaseRepository:
     _constants = RepositoryConstants
 
     # Get SQL Query From File
-    def _getSqlQueryAndFormat(self, filePath: str):
+    def _getSqlQueryFromFile(self, filePath: str):
         with open(path_join(self._constants.SQL_FOLDER_PATH, filePath)) as f:
             query = f.read()
         return query
