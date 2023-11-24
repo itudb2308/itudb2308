c574f323eb02a65f03730a8815017abb18c65f42
Serhat Sari <serhatsari26@hotmail.com>
2023-11-24 17:55:13 +0300

    Implemented home page, improved base template.

--- src/blueprints/UsersBlueprint.py
+++ src/blueprints/UsersBlueprint.py
@@ -1,13 +1,18 @@
 from sqlite3 import Connection
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template
 from repository.UserRepository import UserRepository
 
 def UsersBlueprint(name: str, importName: str, connection: Connection):
     bp = Blueprint(name, importName)
     repository = UserRepository(connection)
 
-    @bp.route('/', methods = ["POST"])
+    @bp.route('/', methods = ["GET"])
     def usersPage():
+        return render_template('users.html')
+
+
+    @bp.route('/searchUsers', methods = ["POST"])
+    def getAll():
         content_type = request.headers.get('Content-Type')
         if (content_type == 'application/json'):
             return repository.getAll(**request.get_json())
