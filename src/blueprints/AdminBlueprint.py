08ec0f589654065f42c94a465a55fcf3d451cd65
Omer Faruk Erdem <mlomer931@gmail.com>
2023-12-02 12:37:00 +0300

    Distribution centers blueprint created and related pages implemented.

--- src/blueprints/AdminBlueprint.py
+++ src/blueprints/AdminBlueprint.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, render_template
 from blueprints.OrdersBlueprint import OrdersBlueprint
 from blueprints.UsersBlueprint import UsersBlueprint
 from blueprints.ProductsBlueprint import ProductsBlueprint
+from blueprints.DistributionCentersBlueprint import DistributionCentersBlueprint
 
 def AdminBlueprint(name: str, importName: str, connection):
     bp = Blueprint(name, importName)
@@ -9,5 +10,5 @@ def AdminBlueprint(name: str, importName: str, connection):
     bp.register_blueprint(OrdersBlueprint("orders", __name__, connection), url_prefix="/orders")
     bp.register_blueprint(UsersBlueprint("users",__name__,connection), url_prefix="/users")
     bp.register_blueprint(ProductsBlueprint("products", __name__, connection), url_prefix="/products")
-
+    bp.register_blueprint(DistributionCentersBlueprint("distributionCenters", __name__, connection), url_prefix="/distributionCenters")
     return bp
---
+++ src/dto/DistributionCenter.py
@@ -0,0 +1,6 @@
+class DistributionCenter:
+    def __init__(self, data):
+        self.id = data[0]
+        self.name = data[1]
+        self.latitude = data[2]
+        self.longitude = data[3]
